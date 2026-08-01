da4eb4cb14e43227f57018b64286778b61c90cb8
BuiHung1696 <minhhungit1696@gmail.com>
2026-08-01 19:09:41 +0700

    build: add all manifest files in .next

---
+++ .next/build/9202cbbbd2128d4f.js
@@ -0,0 +1,6 @@
+var R=require("./chunks/[turbopack]_runtime.js")("9202cbbbd2128d4f.js")
+R.c("chunks/[turbopack-node]_transforms_postcss_ts_0n2egtn._.js")
+R.c("chunks/[root-of-the-server]__0vs85g2._.js")
+R.m("[turbopack-node]/child_process/globals.ts [postcss] (ecmascript)")
+R.m("[turbopack-node]/child_process/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/livestream-crm/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/child_process/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
+module.exports=R.m("[turbopack-node]/child_process/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/livestream-crm/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/child_process/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
